f16fff0f3646f02546bf5f660637ed04c31c762d
David_03 <castellanosroa9@gmail.com>
2025-08-17 14:23:47 -0400

    feat(PRODUCT): Add HasUuids trait and update primary key settings for Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,17 +8,20 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Models\Traits\BelongsToStore;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Product extends Model
 {
 
-    use HasFactory, BelongsToStore;
+    use HasFactory, BelongsToStore, HasUuids;
 
     protected $guarded = [];
 
     protected $hidden = [
         'created_at',
         'updated_at',
+        'category_id',
+        'image'
     ];
 
     protected $casts = [
@@ -26,6 +29,17 @@ class Product extends Model
         'price' => 'decimal:2',
         'stock' => 'integer',
     ];
+
+    protected $primaryKey = 'id';
+    
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    public function getRouteKeyName(): string
+    {
+        return 'id';
+    }
+
 
     public function category(): BelongsTo
     {
@@ -40,5 +54,10 @@ class Product extends Model
     public function images(): HasMany
     {
         return $this->hasMany(ProductImage::class);
+    }
+
+    public function variants(): HasMany
+    {
+        return $this->hasMany(ProductVariant::class);
     }
 }
